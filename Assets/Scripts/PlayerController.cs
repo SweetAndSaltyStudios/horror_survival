b0a1038302e8487c5559597ba9067164767d1094
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 19:08:20 +0300

    Project assets added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera = default;
+    [SerializeField] private float _mouseSensitivity = default;
+
+    private Quaternion _deltaRotation = default;
+    private Vector3 _deltaPosition = default;
+    private Vector2 _mouseLookDirection = default;
+    private Vector2 _movementDirection = default;
+
+    public const float MIN_LOOK_ANGLE = -90f;
+    public const float MAX_LOOK_ANGLE = 90f;
+    
+    [SerializeField] private float _movementSpeed = default;
+    [SerializeField] private float _jumpForce = default;
+    
+    private Rigidbody _rigidbody = default;
+    private PlayerInputActions _inputActions = default;
+
+    private void Awake() => Initialize();
+
+    private void OnEnable() => _inputActions.Enable();
+    private void Start() => Cursor.lockState = CursorLockMode.Locked;
+    private void OnDisable() => _inputActions.Disable();
+
+    private void Update()
+    {
+        _mainCamera.transform.Rotate(-Vector3.right * _mouseLookDirection.y * _mouseSensitivity * Time.deltaTime);    
+    }
+
+    private void FixedUpdate()
+    {
+        if(_mouseLookDirection != Vector2.zero)
+        {
+            _deltaRotation = Quaternion.Euler(Vector3.up * _mouseLookDirection.x * _mouseSensitivity * Time.fixedDeltaTime);
+            _rigidbody.MoveRotation(_rigidbody.rotation * _deltaRotation);
+        }
+
+        if(_movementDirection != Vector2.zero)
+        {
+            _deltaPosition = ((_rigidbody.transform.forward * _movementDirection.y) + (_rigidbody.transform.right * _movementDirection.x)) * _movementSpeed * Time.fixedDeltaTime;
+            _rigidbody.MovePosition(_rigidbody.position + _deltaPosition);
+        }
+    }
+
+    private void Initialize()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _inputActions = new PlayerInputActions();
+
+        _inputActions.Gameplay.Looking.started += Looking_started;
+        _inputActions.Gameplay.Looking.performed += Looking_performed;
+        _inputActions.Gameplay.Looking.canceled += Looking_canceled;
+
+        _inputActions.Gameplay.Movement.started += Movement_started;
+        _inputActions.Gameplay.Movement.performed += Movement_performed;
+        _inputActions.Gameplay.Movement.canceled += Movement_canceled;
+    }
+    
+    private void Movement_started(InputAction.CallbackContext context)
+    {
+    }
+    private void Movement_performed(InputAction.CallbackContext context)
+    {
+        _movementDirection = _inputActions.Gameplay.Movement.ReadValue<Vector2>();   
+    }
+    private void Movement_canceled(InputAction.CallbackContext context)
+    {
+        _movementDirection = Vector2.zero;
+    }
+    private void Looking_started(InputAction.CallbackContext context)
+    {
+    }
+    private void Looking_performed(InputAction.CallbackContext context)
+    {
+        _mouseLookDirection = _inputActions.Gameplay.Looking.ReadValue<Vector2>(); 
+    }
+    private void Looking_canceled(InputAction.CallbackContext context)
+    {
+        _mouseLookDirection = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/PlayerInputActions.cs
@@ -0,0 +1,315 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerInputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""73c4840f-e6f6-4282-b5d8-424d2ee0a8b1"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""617f7d39-f068-43c0-9eba-e9125fd0ba99"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Looking"",
+                    ""type"": ""Value"",
+                    ""id"": ""d23331f0-662c-4186-a04e-7aeb52704324"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Shooting"",
+                    ""type"": ""Button"",
+                    ""id"": ""7020a13a-dd1d-4ec5-af8c-6d8f653797f6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d1939596-b391-4b12-87d6-cfa89398863d"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Looking"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""52ddbc97-a84c-41ac-8d3e-4ba0e506fdaf"",
+                    ""path"": ""<Keyboard>/leftCtrl"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Shooting"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""bb5d4d07-8271-4be5-967d-91ef975e3c20"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""28ac9647-a4f6-4e43-b8b8-61d74ec9a91f"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""d9529926-a84a-4d63-88f9-a87ba00d8993"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""c7c2d8dc-ac47-4311-a371-d07a3696c7f3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""2f44006a-01c6-4eff-a6e2-10761135c43d"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""27890eca-c125-486d-97b7-52c6ad6cf75b"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""f0b49a3b-9bf4-4d29-bbb6-ac8bd65b04b6"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""75fa9e80-193d-4240-be30-498ae761fcb5"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""872648d7-934b-495b-b40e-13efa7325935"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""1ab07c18-2a3e-4f97-a854-63be08a0a48e"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard & Mouse"",
+            ""bindingGroup"": ""Keyboard & Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Movement = m_Gameplay.FindAction("Movement", throwIfNotFound: true);
+        m_Gameplay_Looking = m_Gameplay.FindAction("Looking", throwIfNotFound: true);
+        m_Gameplay_Shooting = m_Gameplay.FindAction("Shooting", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Movement;
+    private readonly InputAction m_Gameplay_Looking;
+    private readonly InputAction m_Gameplay_Shooting;
+    public struct GameplayActions
+    {
+        private @PlayerInputActions m_Wrapper;
+        public GameplayActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Gameplay_Movement;
+        public InputAction @Looking => m_Wrapper.m_Gameplay_Looking;
+        public InputAction @Shooting => m_Wrapper.m_Gameplay_Shooting;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Looking.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLooking;
+                @Looking.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLooking;
+                @Looking.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLooking;
+                @Shooting.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShooting;
+                @Shooting.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShooting;
+                @Shooting.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnShooting;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Looking.started += instance.OnLooking;
+                @Looking.performed += instance.OnLooking;
+                @Looking.canceled += instance.OnLooking;
+                @Shooting.started += instance.OnShooting;
+                @Shooting.performed += instance.OnShooting;
+                @Shooting.canceled += instance.OnShooting;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_KeyboardMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & Mouse");
+            return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnLooking(InputAction.CallbackContext context);
+        void OnShooting(InputAction.CallbackContext context);
+    }
+}
